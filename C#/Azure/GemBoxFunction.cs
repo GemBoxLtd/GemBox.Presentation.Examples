5db0be507d579aad6f4b6f8c1caf0995a83e6c36
Marek Turis <marek.turis@gemboxsoftware.com>
2020-07-02 15:02:15 +0200

    Added azure examples

---
+++ C#/Azure/GemBoxFunction.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using GemBox.Presentation;
+
+public static class GemBoxFunction
+{
+    [FunctionName("GemBoxFunction")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        // If using Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+        var presentation = new PresentationDocument();
+
+        var slide = presentation.Slides.AddNew(SlideLayoutType.Custom);
+
+        var textBox = slide.Content.AddTextBox(ShapeGeometryType.Rectangle, 2, 2, 5, 4, LengthUnit.Centimeter);
+
+        var paragraph = textBox.AddParagraph();
+
+        paragraph.AddRun("Hello World!");
+
+        var fileName = "Output.pptx";
+        var options = SaveOptions.Pptx;
+
+        using (var stream = new MemoryStream())
+        {
+            presentation.Save(stream, options);
+            return new FileContentResult(stream.ToArray(), options.ContentType) { FileDownloadName = fileName };
+        }
+    }
+}
---
+++ VB.NET/Azure/GemBoxFunction.vb
@@ -0,0 +1,37 @@
+Imports System.IO
+Imports Microsoft.AspNetCore.Mvc
+Imports Microsoft.Azure.WebJobs
+Imports Microsoft.Azure.WebJobs.Extensions.Http
+Imports Microsoft.AspNetCore.Http
+Imports Microsoft.Extensions.Logging
+Imports GemBox.Presentation
+
+Module GemBoxFunction
+        <FunctionName("GemBoxFunction")>
+        Async Function Run(
+            <HttpTrigger(AuthorizationLevel.Anonymous, "get", Route := Nothing)> req As HttpRequest,
+            log As ILogger) as Task(Of IActionResult)
+        
+            ' If using Professional version, put your serial key below.
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+            Dim presentation As New PresentationDocument()
+
+            Dim slide = presentation.Slides.AddNew(SlideLayoutType.Custom)
+
+            Dim textBox = slide.Content.AddTextBox(ShapeGeometryType.Rectangle, 2, 2, 5, 4, LengthUnit.Centimeter)
+
+            Dim paragraph = textBox.AddParagraph()
+
+            paragraph.AddRun("Hello World!")
+
+            Dim fileName = "Output.pptx"
+            Dim options = SaveOptions.Pptx
+
+            Using stream As New MemoryStream()
+                presentation.Save(stream, options)
+                Return New FileContentResult(stream.ToArray(), options.ContentType) With { .FileDownloadName = fileName }
+            End Using
+             
+        End Function
+End Module
