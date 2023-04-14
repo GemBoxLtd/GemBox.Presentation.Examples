44c8cf08d76c654fd0c3b43e32462eeb89e43c98
GemBox Dev Team <info@gemboxsoftware.com>
2023-04-14 12:24:54 +0200

    Updated "Getting Started" example.

--- C#/Getting Started/Program.cs
+++ C#/Getting Started/Program.cs
@@ -8,15 +8,12 @@ class Program
         ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 
         var presentation = new PresentationDocument();
-
         var slide = presentation.Slides.AddNew(SlideLayoutType.Custom);
-
         var textBox = slide.Content.AddTextBox(ShapeGeometryType.Rectangle, 2, 2, 5, 4, LengthUnit.Centimeter);
-
         var paragraph = textBox.AddParagraph();
 
         paragraph.AddRun("Hello World!");
 
-        presentation.Save("Hello World.pptx");
+        presentation.Save("HelloWorld.pptx");
     }
 }
--- VB.NET/Getting Started/Program.vb
+++ VB.NET/Getting Started/Program.vb
@@ -8,15 +8,14 @@ Module Program
         ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
         Dim presentation As New PresentationDocument()
-
         Dim slide = presentation.Slides.AddNew(SlideLayoutType.Custom)
-
         Dim textBox = slide.Content.AddTextBox(ShapeGeometryType.Rectangle, 2, 2, 5, 4, LengthUnit.Centimeter)
-
         Dim paragraph = textBox.AddParagraph()
 
         paragraph.AddRun("Hello World!")
 
-        presentation.Save("Hello World.pptx")
+        presentation.Save("HelloWorld.pptx")
+
     End Sub
+
 End Module
