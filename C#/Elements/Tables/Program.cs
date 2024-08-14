68cfcecf5de8f91da44efcc51433c6c3e61be0eb
Mario at GemBox <mario.zorica@gemboxsoftware.com>
2024-08-14 09:50:29 +0200

    Updated GemBox.Presentation example projects.

--- C#/Elements/Tables/Program.cs
+++ C#/Elements/Tables/Program.cs
@@ -5,6 +5,56 @@ class Program
 {
     static void Main()
     {
+        Example1();
+        Example2();
+    }
+
+    static void Example1()
+    {
+        // If using the Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+        var presentation = new PresentationDocument();
+
+        // Create new presentation slide.
+        var slide = presentation.Slides.AddNew(SlideLayoutType.Custom);
+
+        // Create new table.
+        var table = slide.Content.AddTable(5, 5, 20, 12, LengthUnit.Centimeter);
+
+        // Format table with no-style grid.
+        table.Format.Style = presentation.TableStyles.GetOrAdd(
+            TableStyleName.NoStyleTableGrid);
+
+        int columnCount = 4;
+        int rowCount = 10;
+
+        for (int i = 0; i < columnCount; i++)
+            // Create new table column.
+            table.Columns.AddNew(Length.From(5, LengthUnit.Centimeter));
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            // Create new table row.
+            var row = table.Rows.AddNew(
+                Length.From(1.2, LengthUnit.Centimeter));
+
+            for (int j = 0; j < columnCount; j++)
+            {
+                // Create new table cell.
+                var cell = row.Cells.AddNew();
+
+                // Set table cell text.
+                cell.Text.AddParagraph().AddRun(
+                    string.Format(null, "Cell {0}-{1}", i + 1, j + 1));
+            }
+        }
+
+        presentation.Save("Simple Table.pptx");
+    }
+
+    static void Example2()
+    {
         // If using the Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 
@@ -45,7 +95,7 @@ class Program
             | TableStyleOptions.BandedRows;
 
         // Create new table style.
-        var myStyle = presentation.TableStyles.Create("My Table Styles");
+        var myStyle = TableStyleCollection.Create("My Table Styles");
 
         // Get "WholeTable" part style.
         var partStyle = myStyle[TablePartStyleType.WholeTable];
--- VB.NET/Elements/Tables/Program.vb
+++ VB.NET/Elements/Tables/Program.vb
@@ -5,6 +5,56 @@ Module Program
 
     Sub Main()
 
+        Example1()
+        Example2()
+
+    End Sub
+
+    Sub Example1()
+        ' If using the Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY")
+
+        Dim presentation = New PresentationDocument
+
+        ' Create new presentation slide.
+        Dim slide = presentation.Slides.AddNew(SlideLayoutType.Custom)
+
+        ' Create new table.
+        Dim table = slide.Content.AddTable(5, 5, 20, 12, LengthUnit.Centimeter)
+
+        ' Format table with no-style grid.
+        table.Format.Style = presentation.TableStyles.GetOrAdd(
+            TableStyleName.NoStyleTableGrid)
+
+        Dim columnCount As Integer = 4
+        Dim rowCount As Integer = 10
+
+        For i As Integer = 0 To columnCount - 1
+            ' Create new table column.
+            table.Columns.AddNew(Length.From(5, LengthUnit.Centimeter))
+        Next
+
+        For i As Integer = 0 To rowCount - 1
+
+            ' Create new table row.
+            Dim row = table.Rows.AddNew(
+                Length.From(1.2, LengthUnit.Centimeter))
+
+            For j As Integer = 0 To columnCount - 1
+
+                ' Create new table cell.
+                Dim cell = row.Cells.AddNew()
+
+                ' Set table cell text.
+                cell.Text.AddParagraph().AddRun(
+                    String.Format(Nothing, "Cell {0}-{1}", i + 1, j + 1))
+            Next
+        Next
+
+        presentation.Save("Simple Table.pptx")
+    End Sub
+
+    Sub Example2()
         ' If using the Professional version, put your serial key below.
         ComponentInfo.SetLicense("FREE-LIMITED-KEY")
 
@@ -157,4 +207,5 @@ Module Program
 
         presentation.Save("Table Styles.pptx")
     End Sub
+
 End Module
